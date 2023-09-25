127c32604dc91ad41378938eaa1545259e13bb66
PEDRO DUARTE <pedrobradu@gmail.com>
2023-09-25 16:53:51 -0300

    nav links with sense of section location

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import styles from './Header.module.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
+  const [activeSection, setActiveSection] = useState("home");
+
+  useEffect(() => {
+    const handleScroll = () => {
+        const sections = ["home", "experience", "projects", "contact"];
+        for (const section of sections) {
+            const sectionEl = document.getElementById(section);
+            if (sectionEl && sectionEl.getBoundingClientRect().top <= 200) {
+                setActiveSection(section);
+            }
+        }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+        window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body sticky-top">
       <Container>
@@ -14,10 +36,10 @@ function Header() {
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse className="me-5 justify-content-end" id="navbar-nav">
           <Nav className={`${styles.links}`}>
-            <Nav.Link href="#home"><span>Home</span></Nav.Link>
-            <Nav.Link href="#experience"><span>Experience</span></Nav.Link>
-            <Nav.Link href="#projects"><span>Projects</span></Nav.Link>
-            <Nav.Link href="#contact"><span>Contact</span></Nav.Link>
+            <Nav.Link href="#home" className={activeSection === "home" ? `${styles.active}` : ""} active={activeSection === "home"}><span>Home</span></Nav.Link>
+            <Nav.Link href="#experience" className={activeSection === "experience" ? `${styles.active}` : ""} active={activeSection === "experience"}><span>Experience</span></Nav.Link>
+            <Nav.Link href="#projects" className={activeSection === "projects" ? `${styles.active}` : ""} active={activeSection === "projects"}><span>Projects</span></Nav.Link>
+            <Nav.Link href="#contact" className={activeSection === "contact" ? `${styles.active}` : ""} active={activeSection === "contact"}><span>Contact</span></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -26,3 +48,4 @@ function Header() {
 }
 
 export default Header;
+
