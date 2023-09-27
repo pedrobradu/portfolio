481b134aaa062753ecb6d826f233d8702149ec73
PEDRO DUARTE <pedrobradu@gmail.com>
2023-09-27 16:59:52 -0300

    going back to the collapse navbar because the offcanvas dont work in mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,14 +27,6 @@ function Header() {
     };
   }, []);
 
-  const handleNavLinkClick = (e, hash) => {
-    e.preventDefault();
-    setTimeout(() => {
-        console.log("HOIIIIII");
-        window.location.hash = hash;
-    }, 100);  // 150ms delay
-  }
-
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body sticky-top">
       <Container>
@@ -42,7 +34,15 @@ function Header() {
           <p className={`${styles.initials}`}>&lt; PBD/&gt;</p>
         </Navbar.Brand>
           <Navbar.Toggle aria-controls="offcanvas-navbar-nav" />
-          <Navbar.Offcanvas 
+          <Navbar.Collapse className="me-5 justify-content-end" id="navbar-nav">
+              <Nav className={`${styles.links}`}>
+                  <Nav.Link href="#home" className={activeSection === "home" ? `${styles.active}` : ""} active={activeSection === "home"}><span>Home</span></Nav.Link>
+                  <Nav.Link href="#experience" className={activeSection === "experience" ? `${styles.active}` : ""} active={activeSection === "experience"}><span>Experience</span></Nav.Link>
+                  <Nav.Link href="#projects" className={activeSection === "projects" ? `${styles.active}` : ""} active={activeSection === "projects"}><span>Projects</span></Nav.Link>
+                  <Nav.Link href="#contact" className={activeSection === "contact" ? `${styles.active}` : ""} active={activeSection === "contact"}><span>Contact</span></Nav.Link>
+              </Nav>
+          </Navbar.Collapse>
+          {/* <Navbar.Offcanvas 
             id="offcanvas-navbar-nav" 
             aria-labelledby="offcanvas-navbar-label"
             placement="end"
@@ -52,38 +52,33 @@ function Header() {
             </Offcanvas.Header>
             <Offcanvas.Body className="bg-warning bg-opacity-75 text-black justify-content-end">
               <Nav className={`${styles.links}`}>
-                {/* ... your nav links ... */}
                 <Nav.Link 
-                  href="#home" 
-                  onClick={(e) => handleNavLinkClick(e, "#home")}
+                  href="#home"                   
                   className={activeSection === "home" ? `${styles.active}` : ""} 
                   active={activeSection === "home"}>
                     <span>Home</span>
                 </Nav.Link>
                 <Nav.Link 
-                  href="#experience" 
-                  onClick={(e) => handleNavLinkClick(e, "#experience")}
+                  href="#experience"                   
                   className={activeSection === "experience" ? `${styles.active}` : ""} 
                   active={activeSection === "experience"}>
                     <span>Experience</span>
                 </Nav.Link>
                 <Nav.Link 
-                  href="#projects" 
-                  onClick={(e) => handleNavLinkClick(e, "#projects")}
+                  href="#projects"                   
                   className={activeSection === "projects" ? `${styles.active}` : ""} 
                   active={activeSection === "projects"}>
                     <span>Projects</span>
                 </Nav.Link>
                 <Nav.Link 
-                  href="#contact" 
-                  onClick={(e) => handleNavLinkClick(e, "#contact")}
+                  href="#contact"                   
                   className={activeSection === "contact" ? `${styles.active}` : ""} 
                   active={activeSection === "contact"}>
                     <span>Contact</span>
                 </Nav.Link>
               </Nav>
             </Offcanvas.Body>
-          </Navbar.Offcanvas>
+          </Navbar.Offcanvas> */}
       </Container>
     </Navbar>
   );
