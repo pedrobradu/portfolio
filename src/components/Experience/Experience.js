4a7ac395fa60410e40ec262e7895087be2f3076c
PEDRO DUARTE <pedrobradu@gmail.com>
2023-09-29 15:41:56 -0300

    testing transitions

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,29 +1,46 @@
 import React from 'react';
 import styles from './Experience.module.css';
-import { Container, Row, Col, ListGroup, Image } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import kuke from '../../assets/images/kuke.png'
 import novamente from '../../assets/images/novamente.png'
 import lewagon from '../../assets/images/lewagon.png'
 import { useInView } from 'react-intersection-observer';
 
-function Experience({id}) {
-
+function ExperienceItem({ exp }) {
   const [ref, inView] = useInView({
-    triggerOnce: false, // Observe forever
-    threshold: 0.4 // At least 10% of the element is visible
+    triggerOnce: false,
+    threshold: 1
   });
 
+  return (
+    <Row ref={ref} className={`d-flex align-items-center ${styles.experienceItem}`}>
+      <Col md={2} className={`${styles.experienceLogo} ${inView ? "animate__animated animate__fadeInLeft" : "animate__animated animate__fadeOutLeft"}`}>
+          <a href={exp.website} target="_blank" rel="noopener noreferrer">
+              <Image src={exp.logo} alt={`${exp.company} Logo`} fluid rounded />
+          </a>
+      </Col>
+      <Col md={8} className={`${styles.experienceDetail} ${inView ? "animate__animated animate__fadeInUp" : "animate__animated animate__fadeOut"}`}>
+        <div>
+          <h3>{exp.title} at {exp.company}</h3>
+          <p>{exp.description}</p>
+        </div>
+      </Col>
+      <Col md={2} className={`${inView ? "animate__animated animate__fadeInRight" : "animate__animated animate__fadeOutRight"}`}>
+        <span className={styles.experienceDuration}>{exp.duration}</span>
+      </Col>
+    </Row>
+  );
+}
+
+
+function Experience({id}) {
+  
   const experiences = [
     {
       title: "Full Stack Developer",
       company: "Kuke",
       duration: "Feb 2023 - Present",
       description: "Led the business technology transition to a subscription-based model using Python, Django, and AWS. I developed an integrated client and order platform, boosting efficiency, and integrated payment and notification systems like Snipcart, Stripe, and Zapi for streamlined operations.",
-      responsibilities: [
-        "Optimized the web application for mobile devices.",
-        "Collaborated with UX/UI designers to implement responsive designs.",
-        // ... other responsibilities or achievements
-      ],
       logo: kuke, // Path to the logo image of the company
       website: "https://clubekuke.com.br/",
     },
@@ -32,11 +49,6 @@ function Experience({id}) {
       company: "Novamente Software House",
       duration: "Oct 2022 - Jan 2023",
       description: "Directly collaborated with clients to align product development, led robust database creation with PostgreSQL, and employed Ruby on Rails for backend tasks. I also crafted front-end designs using Bootstrap and ensured optimal app performance on Heroku.",
-      responsibilities: [
-        "Optimized the web application for mobile devices.",
-        "Collaborated with UX/UI designers to implement responsive designs.",
-        // ... other responsibilities or achievements
-      ],
       logo: novamente, // Path to the logo image of the company
       website: "https://novamente.cc/",
     },
@@ -45,44 +57,23 @@ function Experience({id}) {
       company: "Le Wagon",
       duration: "Jul 2022 - Sep 2022",
       description: "9-week full-time intensive coding bootcamp learning HTML, CSS, Bootstrap, JavaScript ES6, SQL, Git, GitHub, Heroku and Ruby on Rails. Designed, implemented and shipped to production a clone of AirBnB and a Rails prototype.",
-      responsibilities: [
-        "Optimized the web application for mobile devices.",
-        "Collaborated with UX/UI designers to implement responsive designs.",
-        // ... other responsibilities or achievements
-      ],
       logo: lewagon, // Path to the logo image of the company
       website: "https://www.lewagon.com/",
     },
     // ... other experiences
   ];
 
+  const [sectionRef, sectionInView] = useInView({
+    triggerOnce: false,
+    threshold: 0.9
+  });
+
   return (
-    <section id={id} ref={ref}>
+    <section id={id}>
       <Container className={styles.experienceSection}>
-        <h2 className={`mb-5 ${inView ? "animate__animated animate__fadeInDown" : "animate__animated animate__fadeOut"}`}>Experience</h2>
+        <h2 ref={sectionRef} className={`mb-5 ${sectionInView ? "animate__animated animate__fadeInDown" : "animate__animated animate__fadeOut"}`}>Experience</h2>
         {experiences.map((exp, index) => (
-          <Row key={index} className={`d-flex align-items-center ${styles.experienceItem}`}>
-            <Col md={2} className={`${styles.experienceLogo} ${inView ? "animate__animated animate__fadeInLeft" : "animate__animated animate__fadeOutLeft"}`}>
-                <a href={exp.website} target="_blank" rel="noopener noreferrer">
-                    <Image src={exp.logo} alt={`${exp.company} Logo`} fluid rounded />
-                </a>
-            </Col>
-            <Col md={8} className={`${styles.experienceDetail} ${inView ? "animate__animated animate__fadeInUp" : "animate__animated animate__fadeOut"}`}>
-              <div>
-                <h3>{exp.title} at {exp.company}</h3>
-                <p>{exp.description}</p>
-              </div>
-              {/* <h5>Key Responsibilities:</h5>
-              <ListGroup>
-                {exp.responsibilities.map((res, idx) => (
-                  <ListGroup.Item key={idx}>{res}</ListGroup.Item>
-                ))}
-              </ListGroup> */}
-            </Col>
-            <Col md={2} className={`${inView ? "animate__animated animate__fadeInRight" : "animate__animated animate__fadeOutRight"}`}>
-              <span className={styles.experienceDuration}>{exp.duration}</span>
-            </Col>
-          </Row>
+          <ExperienceItem key={index} exp={exp} />
         ))}
       </Container>
     </section>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,60 +5,69 @@ import locals_1 from '../../assets/images/locals_1.jpg';
 import presentation from '../../assets/images/presentation.jpeg';  
 import { useInView } from 'react-intersection-observer';
 
-
-const projects = [
-  {
-    title: "Locals Only",
-    description: "A social network for local recomendations.",
-    image: locals_1,
-    liveLink: "https://locals-only-pedrobradu-49cce69c5b97.herokuapp.com/",
-    codeLink: "https://github.com/pedrobradu/LOCALS_ONLY",
-  },
-  {
-    title: "Final presentation at Google for Startups.",
-    description: "Graduation presentation for the Le Wagon community.",
-    image: presentation,
-    type: "video",
-    liveLink: "https://www.youtube.com/watch?v=2GYRJtd1Qgc&t=4943s",
-  },
-  // ... other projects
-];
-
-function Projects({id}) {
-
+function ProjectItem({ project }) {
   const [ref, inView] = useInView({
-    triggerOnce: false, // Observe forever
-    threshold: 0.4 // At least 10% of the element is visible
+    triggerOnce: false,
+    threshold: 0.5
   });
 
   return (
-    <section id={id} ref={ref}>
+    <Col md={6} sm={12} ref={ref} className={`${styles.projectCol} ${inView ? "animate__animated animate__fadeIn" : "animate__animated animate__fadeOut"}`}>
+      <Card className={styles.projectCard}>
+        <Card.Img variant="top" src={project.image} className={styles.projectImage} />
+        <Card.Body>
+          <Card.Title>{project.title}</Card.Title>
+          <Card.Text>{project.description}</Card.Text>
+          {project.type === 'video' ? (
+            <Button className="btn-warning" href={project.liveLink} target="_blank">Watch Video</Button>
+          ) : (
+            <>
+              <Button className="btn-warning" href={project.liveLink} target="_blank">WebSite</Button>
+              {/* Only render GitHub button if codeLink is present */}
+              {project.codeLink && (
+                <Button href={project.codeLink} target="_blank" className="ms-2 btn-warning">Github</Button>
+              )}
+            </>
+          )}
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+}
+
+
+function Projects({id}) {
+
+  const projects = [
+    {
+      title: "Locals Only",
+      description: "A social network for local recomendations.",
+      image: locals_1,
+      liveLink: "https://locals-only-pedrobradu-49cce69c5b97.herokuapp.com/",
+      codeLink: "https://github.com/pedrobradu/LOCALS_ONLY",
+    },
+    {
+      title: "Final presentation at Google for Startups.",
+      description: "Graduation presentation for the Le Wagon community.",
+      image: presentation,
+      type: "video",
+      liveLink: "https://www.youtube.com/watch?v=2GYRJtd1Qgc&t=4943s",
+    },
+    // ... other projects
+  ];
+
+  const [sectionRef, sectionInView] = useInView({
+    triggerOnce: false, // Observe forever
+    threshold: 1 // At least 10% of the element is visible
+  });
+
+  return (
+    <section id={id}>
       <Container className={styles.projectsSection}>
-        <h2 className={`mb-5 ${inView ? "animate__animated animate__fadeInDown" : "animate__animated animate__fadeOut"}`}>Projects</h2>
+        <h2 ref={sectionRef} className={`mb-5 ${sectionInView ? "animate__animated animate__fadeInDown" : "animate__animated animate__fadeOut"}`}>Projects</h2>
         <Row>
           {projects.map((project, index) => (
-            <Col md={6} sm={12} key={index} className={`${styles.projectCol} ${inView ? "animate__animated animate__fadeIn" : "animate__animated animate__fadeOut"}`}>
-              <Card className={styles.projectCard}>
-                <Card.Img variant="top" src={project.image} className={styles.projectImage} />
-                <Card.Body>
-                  <Card.Title>{project.title}</Card.Title>
-                  <Card.Text>
-                    {project.description}
-                  </Card.Text>
-                  {project.type === 'video' ? (
-                    <Button className="btn-warning" href={project.liveLink} target="_blank">Watch Video</Button>
-                  ) : (
-                    <>
-                      <Button className="btn-warning" href={project.liveLink} target="_blank">WebSite</Button>
-                      {/* Only render GitHub button if codeLink is present */}
-                      {project.codeLink && (
-                        <Button href={project.codeLink} target="_blank" className="ms-2 btn-warning">Github</Button>
-                      )}
-                    </>
-                  )}
-                </Card.Body>
-              </Card>
-            </Col>
+            <ProjectItem key={index} project={project} />
           ))}
         </Row>
       </Container>
